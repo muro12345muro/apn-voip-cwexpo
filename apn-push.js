8e9c3a72be15e92fc882539fd30a480e4e1b9cd9
muro12345muro <68468074+muro12345muro@users.noreply.github.com>
2023-09-21 17:00:09 +0300

    Update apn-push.js

--- apn-push.js
+++ apn-push.js
@@ -1,35 +1,39 @@
 var apn = require("apn");
 var deviceToken = "5b9aa01e8ec8214f1d7e570ee84d66936bd7af8a13eeaa455b6cbaab5c5aa5fc";
 
-let provider = new apn.Provider( 
-    {
-        token: {
-            key: "AuthKey_HH5BFZJZD4.p8",
-            keyId: "HH5BFZJZD4",
-            teamId: "9U32ZT7NKJ"
-        },
-        production: false
-    });
-console.log(deviceToken, " : sdad_ad_AS");
+// Create a new APNs provider with your credentials
+let provider = new apn.Provider(
+  {
+    token: {
+      key: "AuthKey_HH5BFZJZD4.p8",
+      keyId: "HH5BFZJZD4",
+      teamId: "9U32ZT7NKJ"
+    },
+    production: false // Set to true for production environment
+  });
 
+console.log(deviceToken, " : Device Token");
 
 let notification = new apn.Notification();
 
+// Configure the raw payload for the notification
 notification.rawPayload = {
-    "aps": {
-        "alert": {
-            "uuid": "32323232323",
-            "incoming_caller_id": "123456789",
-            "incoming_caller_name": "Tester",
-        }
+  "aps": {
+    "alert": {
+      "uuid": "32323232323",
+      "incoming_caller_id": "123456789",
+      "incoming_caller_name": "Tester",
     }
+  }
 };
 notification.pushType = "voip";
 notification.topic = "com.mydoup.application.voip";
 
-console.log(notification, " : notification-sdad_ad_AS");
+console.log(notification, " : Notification Payload");
 
-provider.send(notification, deviceToken).then((err, result) => {
-    if (err) return console.log(JSON.stringify(err));
-    return console.log(JSON.stringify(result))
+// Send the notification and handle success or error
+provider.send(notification, deviceToken).then((result) => {
+  console.log("Notification sent successfully:", JSON.stringify(result));
+}).catch((err) => {
+  console.error("Notification sending failed:", JSON.stringify(err));
 });

48f9db0539f047728b3f98c2abac1e1385e1d044
muro12345muro <68468074+muro12345muro@users.noreply.github.com>
2023-09-22 03:13:32 +0300

    Update apn-push.js

--- apn-push.js
+++ apn-push.js
@@ -1,13 +1,13 @@
 var apn = require("apn");
-var deviceToken = "5b9aa01e8ec8214f1d7e570ee84d66936bd7af8a13eeaa455b6cbaab5c5aa5fc";
+var deviceToken = "4810ee474b6942d3ffdb240b67962d89b1e27652478bb6e6e59012d3240c8978";
 
 // Create a new APNs provider with your credentials
 let provider = new apn.Provider({
-    token: {
-      key: "AuthKey_HH5BFZJZD4.p8",
-      keyId: "HH5BFZJZD4",
-      teamId: "9U32ZT7NKJ"
-    },
+    //token: {
+    //  key: "AuthKey_HH5BFZJZD4.p8",
+   //   keyId: "HH5BFZJZD4",
+   //   teamId: "9U32ZT7NKJ"
+  //  },
     production: false // Set to true for production environment
 });
 
@@ -22,10 +22,11 @@ notification.rawPayload = {
             "incoming_caller_name": "Tester",
         }
     }
+    "--cert": "voip_services.pem" // Custom property
 };
 
 notification.pushType = "voip";
-notification.topic = "com.mydoup.application.voip";
+notification.topic = "com.bakiryazilim.BarcodeAppn.voip";
 
 
 console.log(notification, " : Notification Payload");
